0d12dcf7a54d9662ed72dbf8d9566156b5b57c7c
Brody14 <stefania.spinas@gmail.com>
2023-04-26 16:10:59 +0200

    FEAT type migration and seeder

---
+++ database/seeders/TypeSeeder.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Type;
+use Illuminate\Database\Console\Seeds\WithoutModelEvents;
+use Illuminate\Database\Seeder;
+use Illuminate\Support\Str;
+
+class TypeSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        $types = ['HTML', 'CSS', 'JavaScript', 'Php', 'Laravel', 'VueJs'];
+
+        foreach ($types as $type_name) {
+            $new_type = new Type();
+            $new_type->name = $type_name;
+            $new_type->slug = Str::slug($type_name);
+            $new_type->save();
+        }
+    }
+}
